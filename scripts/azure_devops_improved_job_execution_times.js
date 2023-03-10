97bfdf3ebd5e769e4f6aec3951f64182ac542b8b
Marcin Kielar <marcin.kielar@gmail.com>
2023-03-10 11:54:53 +0100

    Fix colors

--- scripts/azure_devops_improved_job_execution_times.js
+++ scripts/azure_devops_improved_job_execution_times.js
@@ -15,13 +15,13 @@
 
     GM_addStyle (`
         a.job-row > span.text-ellipsis, div.stage-card-primary > div.stage-progress-row > span.text-ellipsis {
-            background-color: #005ba1;
+            background-color: #f1f1f1;
             overflow: unset;
             border-radius: 16px;
             padding: 0 8px;
         }
         a.job-row > span.text-ellipsis > span.bolt-time-item, div.stage-card-primary > div.stage-progress-row > span.text-ellipsis > span.bolt-time-item {
-            color: #ffffff !important;
+            color: #767676 !important;
         }
     `)
 })();
