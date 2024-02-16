19dfc287a86487fc662187e5888a5e7c8be028a8
Marcin Kielar <marcin.kielar@gmail.com>
2024-02-16 08:15:42 +0100

    Set @runat to delay CSS mangling

--- scripts/gcloud_column_widths.js
+++ scripts/gcloud_column_widths.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GCloud Columns Width Fix
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Redesigns some GCloud Tables/Columns width to better visualise contents.
 // @author       mkielar
 // @license      MIT
@@ -9,6 +9,7 @@
 // @run-at       document-start
 // @grant        GM_addStyle
 // @grant        window.onurlchange
+// @run-at      document-idle
 // ==/UserScript==
 
 (function() {
