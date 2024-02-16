a738c9eb9e84ae1029c090eb7e9ba84c108cea66
Marcin Kielar <marcin.kielar@gmail.com>
2024-02-16 14:17:26 +0100

    Fix widths for name columns in GCS (and maybe other places)

--- scripts/gcloud_column_widths.js
+++ scripts/gcloud_column_widths.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         GCloud Columns Width Fix
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Redesigns some GCloud Tables/Columns width to better visualise contents.
 // @author       mkielar
 // @license      MIT
 // @match        https://console.cloud.google.com/cloud-build/*
+// @match        https://console.cloud.google.com/storage/*
 // @run-at       document-start
 // @grant        GM_addStyle
 // @grant        window.onurlchange
@@ -18,6 +19,9 @@
     GM_addStyle (`
       .cfc-table-cell-truncation-or-extender-template {
           max-width: 1024px !important;
+      }
+      .name-cell {
+          max-width: fit-content !important;
       }
     `)
 })();
